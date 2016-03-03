7ed4580aa75dc3c2fe867a075c965d7ea0c36fbf
Tschouns <jonas.aklin@gmail.com>
2016-03-03 23:54:05 +0100

    ingore file umdated

---
+++ Blitz/OpenCanvas.cs
@@ -0,0 +1,36 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BiteMe.cs" company="Jonas Aklin">
+//     Copyright (c) Jonas Aklin. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Blitz
+{
+    using System.Windows.Controls;
+    using System.Windows.Media;
+    using Base.RuntimeChecks;
+
+    /// <summary>
+    /// Represents a canvas to which drawings can be added.
+    /// </summary>
+    public class OpenCanvas : Canvas, IOpenCanvas
+    {
+        /// <summary>
+        /// See <see cref="IOpenCanvas.AddDrawing"/>.
+        /// </summary>
+        public void AddDrawing(DrawingVisual drawing)
+        {
+            Checks.AssertNotNull(drawing, nameof(drawing));
+
+            this.AddVisualChild(drawing);
+        }
+
+        /// <summary>
+        /// See <see cref="IOpenCanvas.ClearDrawings"/>.
+        /// </summary>
+        public void ClearDrawings()
+        {
+            this.Children.Clear();
+        }
+    }
+}
