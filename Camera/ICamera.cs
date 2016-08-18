77d0002b8247d3936917a522de786d362db073f1
Tschouns <jonas.aklin@gmail.com>
2016-08-18 13:30:48 +0200

    Camera project added.

---
+++ Camera/ICamera.cs
@@ -0,0 +1,17 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="BiteMe.cs" company="Jonas Aklin">
+//     Copyright (c) Jonas Aklin. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Camera
+{
+    /// <summary>
+    /// Represents a camera pointed at the game world. Helps with transforming positions
+    /// between world and display coordinates.
+    /// </summary>
+    public interface ICamera
+    {
+
+    }
+}
